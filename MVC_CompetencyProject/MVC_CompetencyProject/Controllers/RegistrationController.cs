aad275c3998169fbb03bbde9fe95717e1202f51d
mighty13me <harshunu@yahoo.com>
2020-07-13 17:10:59 -0500

    Second Set of Changes

---
+++ MVC_CompetencyProject/MVC_CompetencyProject/Controllers/RegistrationController.cs
@@ -0,0 +1,19 @@
+﻿using MVC_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_CompetencyProject.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Registration()
+        {
+            UserDetail reg = new UserDetail();
+            return View();
+        }
+    }
+}
---
+++ MVC_CompetencyProject/MVC_CompetencyProject/Infrastructure/Respository/Registration.cs
@@ -0,0 +1,16 @@
+﻿using MVC_CompetencyProject.Infrastructure.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CompetencyProject.Infrastructure.Respository
+{
+    public class Registration : IUserActions
+    {
+        public void RegistrationPage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
