91cbb2c0085c0399aa35dda0fe9b1f43a6e94e54
mighty13me <harshunu@yahoo.com>
2020-07-13 07:59:58 -0500

    Working plan for NTier Architecture

---
+++ MVC_CompetencyProject/MVC_Model/UserDetail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Model
+{
+    public class UserDetail
+    {
+        [Key]
+        public int UserID { get; set; }
+        public string LoginName { get; set; }
+        public string PasswordHash { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CountryID { get; set; }
+        public string Languages { get; set; }
+        public string Gender { get; set; }
+        public int Address { get; set; }
+
+
+    }
+}
